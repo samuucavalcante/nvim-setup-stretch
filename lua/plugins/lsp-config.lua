be3f39a05a69416378caad527ee1b4a5b4db12ac
Samuel Cavalcante <samuel-icm-dantas@hotmail.com>
2025-05-08 23:50:13 -0300

    remove plugin

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -10,7 +10,7 @@ return {
     config = function()
       require("mason-lspconfig").setup({
         automatic_enable = false,
-        ensure_installed = { "lua_ls", "vtsls" }
+        ensure_installed = { "lua_ls", "ts_ls" }
       })
     end
   },
@@ -19,6 +19,9 @@ return {
     config = function()
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
+      lspconfig.ts_ls.setup({
+      })
+
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
       vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
