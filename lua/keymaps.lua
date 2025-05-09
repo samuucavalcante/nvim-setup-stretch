5cb7c409da6fa5fea8fe462a19609aebeac6c04c
Samuel Cavalcante <samuel-icm-dantas@hotmail.com>
2025-05-09 01:12:45 -0300

    feat: add debugging

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,3 +1,10 @@
 vim.keymap.set('n', '<C-s>', ':w<CR>', { noremap = true })
 vim.keymap.set('i', '<C-s>', '<Esc>:w<CR>gi', { noremap = true })
 vim.keymap.set('v', '<C-s>', '<Esc>:w<CR>gv', { noremap = true })
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+pattern = "*",
+callback = function()
+vim.lsp.buf.format({ async = false })
+end,
+})
